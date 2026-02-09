e500696309c9401ad77aaaceb2f83c7499d6282c
Davi-Oliveira214 <davi.santosoliveira214@gmail.com>
2026-02-09 08:44:13 -0300

    Teste dos novos métodos

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
 import excecoes.ExcecoesLivro;
-import gerenciamento.LivroConfig;
+import gerenciamento.LivroControle;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        LivroConfig lc = new LivroConfig();
+        LivroControle lc = new LivroControle();
 
         try {
-            lc.addLivro("Teste", "Teste", 200, 1842, 19, LocalDate.now());
-            System.out.println(lc.buscarLivro("Teste"));
+            lc.addLivro("Teste", "Teste", 200, 1945, 50, LocalDate.now());
 
-            lc.delLivro(1842);
+            lc.altTitulo(1945, "Teste2");
+            lc.altAutor(1945, "Teste2");
+            lc.altPreco(1945, 300);
+            lc.altEstoque(1945, 4000);
+            lc.altISBN(1945, 1978);
+
+            System.out.println(lc.buscarPreco(300));
+            lc.delLivro(1978);
         } catch (ExcecoesLivro e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println(lc.getLivros());
     }
 }
