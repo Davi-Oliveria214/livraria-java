996380644ee58bc8206c5b16c52f501cc705002a
Davi-Oliveira214 <davi.santosoliveira214@gmail.com>
2026-02-22 08:27:42 -0300

    Organizando a arquitetuura do sistema

--- src/cli/LivroCLI.java
+++ src/cli/LivroCLI.java
@@ -7,14 +7,12 @@ import excecoes.ExcecoesLivro;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class LivroCLI {
     private final LivroController livroControle;
     private final Scanner sc;
     private final DateTimeFormatter formato;
-    private LivroEntity livroEntity;
 
     public LivroCLI(LivroController livroController) {
         this.livroControle = livroController;
@@ -36,18 +34,19 @@ public class LivroCLI {
                         this.addLivro();
                         break;
                     case 2:
-
+                        this.livroControle.verificar();
                         this.delLivro();
                         break;
                     case 3:
-
+                        this.livroControle.verificar();
                         this.buscarLivro();
                         break;
                     case 4:
-
+                        this.livroControle.verificar();
                         this.altLivro();
                         break;
                     case 5:
+                        this.livroControle.verificar();
                         this.todosLivros();
                         break;
                     case 0:
@@ -90,7 +89,7 @@ public class LivroCLI {
             String lancamento = sc.nextLine();
             LocalDate data = LocalDate.parse(lancamento, formato);
 
-
+            this.livroControle.addLivro(titulo, autor, preco, isbn, estoque, data);
         } catch (ExcecoesLivro e) {
             System.out.println(e.getMessage());
         } catch (InputMismatchException in) {
@@ -108,16 +107,16 @@ public class LivroCLI {
             System.out.println("Digite a ISBN do livro");
             int isbn = sc.nextInt();
 
+            LivroEntity livro = this.livroControle.buscarISBN(isbn);
 
-
-            if (livroEntity != null) {
+            if (livro != null) {
                 sc.nextLine();
-                System.out.println("Deseja deletar o livro: " + livroEntity.toString() + "\n 1-Sim \n 2-Não");
+                System.out.println("Deseja deletar o livro: " + livro + "\n 1-Sim \n 2-Não");
                 opcao = sc.nextInt();
             }
 
             if (opcao == 1) {
-
+                this.livroControle.delLivro(isbn);
                 System.out.println("Livro deletado com sucesso!!!");
             }
         } catch (ExcecoesLivro e) {
@@ -136,37 +135,43 @@ public class LivroCLI {
                 System.out.println("Escolha a maneira que deseja procura o livro \n 1-Titulo \n 2-Autor \n 3-ISBN \n 4-Preço \n 0-Menu");
                 opcao = sc.nextInt();
 
+                List<LivroEntity> livros = new ArrayList<>();
+
                 sc.nextLine();
                 switch (opcao) {
                     case 1:
                         System.out.println("Digite o titulo do livro");
                         String titulo = sc.nextLine();
 
-
+                        livros = this.livroControle.buscarTitulo(titulo);
                         break;
                     case 2:
                         System.out.println("Digite o nome do autor que deseja encontrar");
                         String autor = sc.nextLine();
 
-
+                        livros = this.livroControle.buscarAutor(autor);
                         break;
                     case 3:
                         System.out.println("Digite a ISBN do livro que deseja encontrar");
                         int isbn = sc.nextInt();
 
-
+                        livros = Collections.singletonList(this.livroControle.buscarISBN(isbn));
                         break;
                     case 4:
                         System.out.println("Digite o preço que deseja encontrar");
                         double preco = sc.nextDouble();
 
-
+                        livros = this.livroControle.buscarPreco(preco);
                         break;
                     case 0:
                         System.out.println("Voltando ao menu inicial");
                         break;
                     default:
                         System.out.println("Nenhuma opção encontrada");
+                }
+
+                if (opcao != 0) {
+                    System.out.println("Livros encontrados\n" + livros.toString());
                 }
             } catch (ExcecoesLivro e) {
                 System.out.println(e.getMessage());
@@ -185,9 +190,9 @@ public class LivroCLI {
                 System.out.println("Digite a ISBN do livro que deseja alterar");
                 int isbn = sc.nextInt();
 
+                LivroEntity livro = livroControle.buscarISBN(isbn);
 
-
-                System.out.println("\n" + livroEntity.toString() + "\n");
+                System.out.println("\n" + livro.toString() + "\n");
 
                 sc.nextLine();
                 System.out.println("Escolha o que deseja alterar do livro \n 1-Titulo \n 2-Autor \n 3-Preço \n 4-Estoque \n 5-ISBN \n 0-Menu principal");
@@ -199,35 +204,35 @@ public class LivroCLI {
                         System.out.println("Digite o novo titulo");
                         String titulo = sc.nextLine();
 
-
+                        this.livroControle.altTitulo(isbn, titulo);
                         System.out.println("Titulo alterado com sucesso");
                         break;
                     case 2:
                         System.out.println("Digite o novo autor");
                         String autor = sc.nextLine();
 
-
+                        this.livroControle.altAutor(isbn, autor);
                         System.out.println("Autor alterado com sucesso");
                         break;
                     case 3:
                         System.out.println("Digite o novo preço");
                         double preco = sc.nextDouble();
 
-
+                        this.livroControle.altPreco(isbn, preco);
                         System.out.println("Preço alterado com sucesso");
                         break;
                     case 4:
                         System.out.println("Digite o novo Estoque");
                         int estoque = sc.nextInt();
 
-
+                        this.livroControle.altEstoque(isbn, estoque);
                         System.out.println("Estoque alterado com sucesso");
                         break;
                     case 5:
                         System.out.println("Digite a nova ISBN");
                         int isbnNova = sc.nextInt();
 
-
+                        this.livroControle.altISBN(isbn, isbnNova);
                         System.out.println("ISBN alterado com sucesso");
                         break;
                     case 0:
@@ -237,9 +242,9 @@ public class LivroCLI {
                         System.out.println("Nenhuma opção encontrada");
                 }
 
-                System.out.println("Novas informações: " + livroEntity.toString());
+                System.out.println("Novas informações: " + livro);
 
-                System.out.println("Deseja fazer alguma outra alteração?");
+                System.out.println("Deseja fazer alguma outra alteração?\n 0-Não 1-Sim");
                 opcao = sc.nextInt();
             } catch (ExcecoesLivro e) {
                 System.out.println(e.getMessage());
@@ -252,7 +257,7 @@ public class LivroCLI {
 
     private void todosLivros() {
         try {
-
+            System.out.println(this.livroControle.mostarLivros().toString());
         } catch (ExcecoesLivro e) {
             System.out.println(e.getMessage());
         }
--- src/controller/LivroController.java
+++ src/controller/LivroController.java
@@ -5,23 +5,66 @@ import excecoes.ExcecoesLivro;
 import services.LivroService;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class LivroController {
     private LivroService service;
     private Long id = 1L;
 
     public LivroController(LivroService livroService) {
-        service = livroService;
+        this.service = livroService;
     }
 
-    public void addLivro(String titulo, String autor, double preco, int isbn, int estoque, LocalDate lancamento) {
-        try {
-            LivroEntity livroEntity = new LivroEntity(id++, titulo, autor, preco, isbn, estoque, lancamento);
-            service.criarLivro(livroEntity);
+    public void addLivro(String titulo, String autor, double preco, int isbn, int estoque, LocalDate lancamento) throws ExcecoesLivro {
+        LivroEntity livroEntity = new LivroEntity(id++, titulo, autor, preco, isbn, estoque, lancamento);
+        this.service.criarLivro(livroEntity);
+    }
 
-            System.out.println("Sucesso: Livro cadastrado!!!");
-        } catch (ExcecoesLivro e) {
-            System.out.println("Erro: " + e.getMessage());
-        }
+    public void delLivro(int isbn) throws ExcecoesLivro {
+        this.service.delLivro(isbn);
+    }
+
+    public List<LivroEntity> mostarLivros() throws ExcecoesLivro {
+        return this.service.getLivros();
+    }
+
+    public List<LivroEntity> buscarTitulo(String titulo) throws ExcecoesLivro {
+        return this.service.buscarTitulo(titulo);
+    }
+
+    public LivroEntity buscarISBN(int isbn) throws ExcecoesLivro {
+        return this.service.buscarISBN(isbn);
+    }
+
+    public List<LivroEntity> buscarAutor(String autor) throws ExcecoesLivro {
+        return this.service.buscarAutor(autor);
+    }
+
+    public List<LivroEntity> buscarPreco(double preco) throws ExcecoesLivro {
+        return this.service.buscarPreco(preco);
+    }
+
+    public void altTitulo(int isbn, String novoTitulo) throws ExcecoesLivro {
+        this.service.altTitulo(isbn, novoTitulo);
+    }
+
+    public void altAutor(int isbn, String novoAutor) throws ExcecoesLivro {
+        this.service.altAutor(isbn, novoAutor);
+    }
+
+    public void altISBN(int isbn, int novaISBN) throws ExcecoesLivro {
+        this.service.altISBN(isbn, novaISBN);
+    }
+
+    public void altPreco(int isbn, double preco) {
+        this.service.altPreco(isbn, preco);
+    }
+
+    public void altEstoque(int isbn, int estoque) {
+        this.service.altEstoque(isbn, estoque);
+    }
+
+    public void verificar() throws ExcecoesLivro {
+        this.service.verificar();
     }
 }
