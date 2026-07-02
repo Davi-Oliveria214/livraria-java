a8c93a38d04c4a2677495a0940cf31fca6a63f42
davil <davi.santosoliveira214@gmail.com>
2026-07-02 20:09:22 -0300

    docs(sql): modifica o script do banco de dados

--- docs/bd_livraria_script.sql
+++ docs/bd_livraria_script.sql
@@ -8,75 +8,88 @@ create type tipo_generos as enum (
 'terror',
 'comedia',
 'drama',
-'misterio');
+'misterio',
+'suspense');
 
 create table generos(
-id integer generated always as identity unique,
-nome tipo_generos unique 
-);
-
-create table tb_livro (
-id bigint generated always as identity unique,
-titulo varchar(255),
-autor varchar(150),
-preco decimal(10, 2),
-isbn varchar(15) unique,
-estoque integer,
-sinopse varchar(350),
-genero integer default 1,
-foreign key (genero) references generos(id),
-unique(titulo, autor)
+                        codigo varchar(5) primary key,
+                        nome tipo_generos unique
 );
 
 insert
-	into
-	generos(nome)
+into
+    generos(codigo, nome)
 values
-('indefinido'),
-('acao'),
-('romance'),
-('aventura'),
-('terror'),
-('comedia'),
-('drama'),
-('misterio');
+    ('ptI10',
+     'indefinido'),
+    ('ptA04',
+     'acao'),
+    ('ptR07',
+     'romance'),
+    ('ptA08',
+     'aventura'),
+    ('ptT06',
+     'terror'),
+    ('ptC07',
+     'comedia'),
+    ('ptD05',
+     'drama'),
+    ('ptM08',
+     'misterio'),
+    ('ptS08',
+     'suspense');
+
+create table tb_livros (
+                           id bigint generated always as identity unique,
+                           titulo varchar(255),
+                           autor varchar(150),
+                           preco decimal(10, 2),
+                           isbn varchar(15) unique,
+                           estoque integer,
+                           sinopse varchar(350),
+                           genero varchar(5) default 'ptI10',
+                           lancamento timestamp,
+                           criado_em timestamptz default now(),
+                           foreign key (genero) references generos(codigo),
+                           unique(titulo, autor)
+);
 
 insert
-	into
-	tb_livro(titulo, autor, preco, isbn, estoque, sinopse, genero)
-values 
-('Livro1',
-'Autor1',
-50.55,
-'534789',
-200,
-'Apenas um livro',
-2),
-('Livro2',
-'Autor1',
-95.55,
-'934282',
-350,
-'Apenas um livro',
-4),
-('Livro1',
-'Autor2',
-75.55,
-'521389',
-155,
-'Apenas um livro',
-5),
-('Livro2',
-'Autor2',
-97.55,
-'544829',
-200,
-'Apenas um livro',
-3),
-('Livro1',
-'Autor3',
-125.55,
-'3474723',
-200,
-'Apenas um livro',
-1);
+into
+    tb_livros(titulo, autor, preco, isbn, estoque, sinopse, genero)
+values
+    ('Livro1',
+     'Autor1',
+     50.55,
+     '534789',
+     200,
+     'Apenas um livro',
+     'ptA04'),
+    ('Livro2',
+     'Autor1',
+     95.55,
+     '934282',
+     350,
+     'Apenas um livro',
+     'ptA08'),
+    ('Livro1',
+     'Autor2',
+     75.55,
+     '521389',
+     155,
+     'Apenas um livro',
+     'ptT06'),
+    ('Livro2',
+     'Autor2',
+     97.55,
+     '544829',
+     200,
+     'Apenas um livro',
+     'ptR07'),
+    ('Livro1',
+     'Autor3',
+     125.55,
+     '3474723',
+     200,
+     'Apenas um livro',
+     'ptI10');
